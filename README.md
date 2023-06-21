a0eb54f023f4699568f477e412343bc61af28de7
Walber Ruthierre da Costa <67650098+ruthierre@users.noreply.github.com>
2023-06-21 15:34:34 +0000

    adicionado o `Header`

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hackathon-frontend
 Frontend do projeto hackathon para Instituto Federal de Sergipe
+
+https://hackathon-frontend-eta.vercel.app/
+
+link para acesso ao projeto online.
--- pages/header.js
+++ pages/header.js
@@ -1,9 +1,12 @@
 export default function Header() {
     return (
-        <nav>
-            <button>AJUDA</button>
-            <button>FAQ</button>
-            <button>Planos</button>
+        <nav className=" bg-yellow-200 box-border h-16 flex justify-end items-center
+        bg-gradient-to-r from-yellow-500  via-red-400   to-pink-500" p-6>
+            <div className=" justify-around space-x-8 px-16">
+                <button  className=" text-white text-xl">AJUDA</button>
+                <button  className=" text-white text-xl">FAQ</button>
+                <button  className=" text-white text-xl">Planos</button>   
+            </div> 
         </nav>
     )
 }
